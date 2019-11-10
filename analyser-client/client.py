3fe0e2b471c2047322db868d1d8dd4b7de770184
Lawrence Berry <lawrenceberry@outlook.com>
2019-11-10 20:37:37 +0000

    Parse status header on connect

--- analyser-client/client.py
+++ analyser-client/client.py
@@ -1,10 +1,7 @@
 import socket
 
-HOST = "10.0.0.126"
-PORT = 1852
 
-
-class AnalyserClient:
+class x30Client:
     """
     Client to interact with an sm130 fibre optic analyser box. Reflects the TCP protocol as far as possible.
     See the Micron Optics User Guide, Revision 1.139 section 4.4.4.5 for details.
@@ -13,27 +10,50 @@ class AnalyserClient:
     ACKNOWLEDGEMENT_LENGTH = 10  # bytes
     STATUS_HEADER_LENGTH = 88  # bytes
 
-    def __init__(self, host: str = HOST, port: int = PORT):
-        self.host = host
-        self.port = port
+    def __init__(self):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    def start(self):
-        self.socket.connect((self.host, self.port))
+        # State
+        self.host = "10.0.0.126"
+        self.port = 1852
 
-    def end(self):
+    def connect(self):
+        self.socket.connect((self.host, self.port))
+        response = self.get_data()
+        status_header = response[:88]
+        self.update_status(status_header)
+
+    def close(self):
         self.socket.close()
 
-    def read(self, override_length: int = None):
+    def read(self, override_length: int = None) -> bytes:
         length = int(self.socket.recv(self.ACKNOWLEDGEMENT_LENGTH))
         if length is not None:  # Override message length if provided
             length = override_length
         response = self.socket.recv(length)
         return response
 
-    def execute(self, command: bytes, override_length: int = None):
+    def execute(self, command: bytes, override_length: int = None) -> bytes:
         self.socket.sendall(command + b"\n")
         return self.read(override_length)
+
+    def update_status(self, status_header: bytes):
+        self.fs_radix = int(status_header[0:8], 2)
+        self.fw_version = status_header[16:24].decode("ascii")
+        self.secondary_fan = bool(status_header[27] == b"1")
+        self.primary_fan = bool(status_header[28] == b"1")
+        self.calibration_fault = bool(status_header[30] == b"1")
+        self.switch_position = int(status_header[48:50], 2)
+        self.mux_level = int(status_header[49:51], 2)
+        self.triggering_mode = int(status_header[52:54], 2)
+        self.operating_mode = int(status_header[54:56], 2)
+        self.num_peaks_detected = status_header[128:192]
+        self.error = status_header[376:384].decode("ascii")
+        self.buffer = status_header[384:392].decode("ascii")
+        self.header_version = status_header[392:400].decode("ascii")
+        self.granularity = int(status_header[576:608], 2)
+        self.full_spectrum_start_wvl = int(status_header[640:672], 2)
+        self.full_spectrum_end_wvl = int(status_header[672:], 2)
 
     def get_data(self):
         return self.execute(b"#GET_DATA")
