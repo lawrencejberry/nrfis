66372d4fd72c8fe25288ffb7b064f31d46177023
Lawrence Berry <lawrenceberry@outlook.com>
2020-02-02 14:44:05 +0000

    API title and description

--- backend/web_server/main.py
+++ backend/web_server/main.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 
 from .routers import fbg
 
-app = FastAPI()
+app = FastAPI(
+    title="NRFIS API",
+    description="An API to access sensor data the University of Cambridge's Civil Engineering building",
+    version=0.1,
+)
 
 app.include_router(fbg.router, prefix="/fbg", tags=["FBG"])
