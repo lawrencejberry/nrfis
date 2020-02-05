d224e605b70d0bfce9c2b70376134b125e07e053
Lawrence Berry <lawrenceberry@outlook.com>
2020-02-05 22:17:54 +0000

    Test data collection system against SQLite database

---
+++ backend/database_models/utils.py
@@ -0,0 +1,80 @@
+from os import path, remove
+import csv
+from datetime import datetime
+import re
+
+from . import (
+    Base,
+    Basement,
+    StrongFloor,
+    SteelFrame,
+    BasementMetadata,
+    StrongFloorMetadata,
+    SteelFrameMetadata,
+)
+
+
+def make_test_db(DATABASE_URL, db, Session):
+    filepath = re.search("///([^;]*)$", DATABASE_URL)[1]
+
+    if path.isfile(filepath):
+        remove(filepath)
+
+    session = Session()
+
+    Base.metadata.create_all(db)
+
+    with open(
+        "backend/database_models/test_data/basement_fbg_metadata.csv",
+        encoding="utf-8-sig",
+    ) as csvfile:
+        data = csv.DictReader(csvfile)
+        for row in data:
+            session.add(BasementMetadata(**{k: v for k, v in row.items() if v != ""}))
+
+    with open(
+        "backend/database_models/test_data/strong_floor_fbg_metadata.csv",
+        encoding="utf-8-sig",
+    ) as csvfile:
+        data = csv.DictReader(csvfile)
+        for row in data:
+            session.add(
+                StrongFloorMetadata(**{k: v for k, v in row.items() if v != ""})
+            )
+
+    with open(
+        "backend/database_models/test_data/steel_frame_fbg_metadata.csv",
+        encoding="utf-8-sig",
+    ) as csvfile:
+        data = csv.DictReader(csvfile)
+        for row in data:
+            session.add(SteelFrameMetadata(**{k: v for k, v in row.items() if v != ""}))
+
+    session.commit()
+
+    with open(
+        "backend/database_models/test_data/basement_fbg.csv", encoding="utf-8-sig"
+    ) as csvfile:
+        data = csv.DictReader(csvfile)
+        for row in data:
+            row["timestamp"] = datetime.fromisoformat(row["timestamp"])
+            session.add(Basement(**{k: v for k, v in row.items() if v != ""}))
+
+    with open(
+        "backend/database_models/test_data/strong_floor_fbg.csv", encoding="utf-8-sig",
+    ) as csvfile:
+        data = csv.DictReader(csvfile)
+        for row in data:
+            row["timestamp"] = datetime.fromisoformat(row["timestamp"])
+            session.add(StrongFloor(**{k: v for k, v in row.items() if v != ""}))
+
+    with open(
+        "backend/database_models/test_data/steel_frame_fbg.csv", encoding="utf-8-sig",
+    ) as csvfile:
+        data = csv.DictReader(csvfile)
+        for row in data:
+            row["timestamp"] = datetime.fromisoformat(row["timestamp"])
+            session.add(SteelFrame(**{k: v for k, v in row.items() if v != ""}))
+
+    session.commit()
+    session.close()
