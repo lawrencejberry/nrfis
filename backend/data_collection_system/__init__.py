a07721f74a0f8e8cadd92fad653437cf68edec5a
Lawrence Berry <lawrenceberry@outlook.com>
2020-02-11 15:39:35 +0000

    Run DCS from root directory

--- backend/data_collection_system/__init__.py
+++ backend/data_collection_system/__init__.py
@@ -6,7 +6,7 @@ import logging.handlers
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from database_models import (
+from backend.database_models import (
     Base,
     basement_package,
     strong_floor_package,
