6241408a76c802a9ba7ea89306d6ec897c56d53f
Lawrence Berry <lawrenceberry@outlook.com>
2020-02-24 23:48:59 +0000

    Visualise steel frame model

--- frontend/app/App.js
+++ frontend/app/App.js
@@ -2,7 +2,7 @@ import React, { Suspense, useState, useEffect } from 'react';
 import { Canvas } from "react-three-fiber"
 import { Asset } from "expo-asset";
 
-import Model from './Model'
+import Model from './SteelFrame'
 
 
 window.performance = {
@@ -18,19 +18,19 @@ export default function App() {
 
   useEffect(() => {
     (async () => {
-      const asset = Asset.fromModule(require("./assets/models/duck.glb"))
+      const asset = Asset.fromModule(require("./assets/models/steel-frame.glb"))
       await asset.downloadAsync()
       setUri(asset.localUri)
     })()
   }, [])
 
   return (
-    <Canvas camera={{ position: [0, 0, 50] }}>
+    <Canvas camera={{ position: [0, 0, 80] }}>
       <ambientLight intensity={0.5} />
       <spotLight intensity={0.8} position={[300, 300, 400]} />
       <Suspense fallback={<></>}>
         {(uri) ? (
-          <Model uri={uri} />
+          <Model uri={uri} position={[0, -20, 0]} />
         ) : null}
       </Suspense>
     </Canvas>
