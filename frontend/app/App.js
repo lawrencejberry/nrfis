023ea4be41a24239ac414f26d056f23d50e08438
Lawrence Berry <lawrenceberry@outlook.com>
2020-02-24 18:17:29 +0000

    Bundle and load local asset URL

--- frontend/app/App.js
+++ frontend/app/App.js
@@ -1,6 +1,9 @@
-import React, { Suspense } from 'react';
-import { Canvas } from "react-three-fiber";
+import React, { Suspense, useState, useEffect } from 'react';
+import { Canvas } from "react-three-fiber"
+import { Asset } from "expo-asset";
+
 import Model from './Model'
+
 
 window.performance = {
   clearMeasures: () => { },
@@ -11,12 +14,24 @@ window.performance = {
 }
 
 export default function App() {
+  const [uri, setUri] = useState("")
+
+  useEffect(() => {
+    (async () => {
+      const asset = Asset.fromModule(require("./assets/models/duck.glb"))
+      await asset.downloadAsync()
+      setUri(asset.localUri)
+    })()
+  }, [])
+
   return (
     <Canvas camera={{ position: [0, 0, 50] }}>
       <ambientLight intensity={0.5} />
       <spotLight intensity={0.8} position={[300, 300, 400]} />
-      <Suspense fallback={null}>
-        <Model />
+      <Suspense fallback={<></>}>
+        {(uri) ? (
+          <Model uri={uri} />
+        ) : null}
       </Suspense>
     </Canvas>
   )
