d300ca99024dca5b52671de7a7a105bb7ee48201
Lawrence Berry <lawrenceberry@outlook.com>
2020-02-27 14:07:53 +0000

    Fix database_models import

--- backend/data_collection_system/__init__.py
+++ backend/data_collection_system/__init__.py
@@ -6,7 +6,7 @@ import logging.handlers
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from database_models import Base, Packages
+from backend.database_models import Base, Packages
 
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
