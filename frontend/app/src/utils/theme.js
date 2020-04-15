f0ac29be56126e6ab4962907aae4b1e8b290fc91
Lawrence Berry <lawrenceberry@outlook.com>
2020-04-15 01:22:11 +0100

    Platform specific fonts

--- frontend/app/src/utils/theme.js
+++ frontend/app/src/utils/theme.js
@@ -1,3 +1,7 @@
+import { Platform } from "react-native";
+
+const font = Platform.select({ default: "Roboto", ios: "Helvetica" });
+
 const colors = {
   primary: "grey", // "#737f8a"
   secondary: "#404040", // Header bar colour
@@ -11,7 +15,7 @@ const theme = {
   Text: {
     style: {
       fontSize: 20,
-      fontFamily: Platform.select({ default: "Roboto", ios: "Helvetica" })
+      fontFamily: font
     }
   },
   Divider: { style: { margin: 4, backgroundColor: colors.border } },
@@ -24,10 +28,11 @@ const theme = {
     },
     buttonStyle: { borderWidth: 1 },
     titleStyle: {
-      fontSize: 16
+      fontSize: 16,
+      fontFamily: font
     }
   },
-  ButtonGroup: { textStyle: { fontSize: 16 } }
+  ButtonGroup: { textStyle: { fontSize: 16, fontFamily: font } }
 };
 
 export default theme;
