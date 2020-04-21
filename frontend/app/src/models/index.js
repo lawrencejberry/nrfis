551360e1717008b7a74ecb5743954091047667f8
Lawrence Berry <lawrenceberry@outlook.com>
2020-04-21 22:08:48 +0100

    Added StrongFloor screen and model

--- frontend/app/src/models/index.js
+++ frontend/app/src/models/index.js
@@ -1,2 +1,3 @@
 export { default as LoadingIndicator } from "./LoadingIndicator";
 export { default as SteelFrameModel } from "./SteelFrameModel";
+export { default as StrongFloorModel } from "./StrongFloorModel";
