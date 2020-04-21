130ed73c255b2cf0b97b37c10582ace5ceca8955
Lawrence Berry <lawrenceberry@outlook.com>
2020-04-21 17:10:10 +0100

    Allow mapping multiple sensor names to an object being coloured

--- frontend/app/src/models/utils.js
+++ frontend/app/src/models/utils.js
@@ -1,11 +1,29 @@
 import React from "react";
 
-export function renderSensorColour(sensorColours, sensor) {
-  if (sensorColours) {
-    return (
-      <meshBasicMaterial attach="material" color={sensorColours[sensor]} />
-    );
-  } else {
-    return null;
+/*
+This function return a Three.js material coloured according to the map
+of sensor colours and a given list of sensor names. The function will try accessing
+the map with each name until it finds a match, at which point it will return a material
+of that colour.
+
+In this way, a Three.js object which represents both a strain and a temperature sensor
+can be coloured just by supplying the names used in both cases to the sensorNames array.
+This avoids having to check the dataType, and allows us to pass in multiple alternative
+names for each sensor in case they change in a future version of the NRFIS API.
+*/
+
+export function renderSensorColour(sensorColours, sensorNames) {
+  sensorNames = Array.isArray(sensorNames) ? sensorNames : [sensorNames]; // To allow specifying a single sensor name not as an array
+
+  for (const sensorName of sensorNames) {
+    if (sensorColours[sensorName]) {
+      return (
+        <meshBasicMaterial
+          attach="material"
+          color={sensorColours[sensorName]}
+        />
+      );
+    }
   }
+  return null;
 }
