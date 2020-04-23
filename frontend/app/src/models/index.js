c74d41eadec1b959def045030c1bab1849bb64e4
Lawrence Berry <lawrenceberry@outlook.com>
2020-04-23 14:11:41 +0100

    Add basement screen and model

--- frontend/app/src/models/index.js
+++ frontend/app/src/models/index.js
@@ -1,3 +1,4 @@
 export { default as LoadingIndicator } from "./LoadingIndicator";
 export { default as SteelFrameModel } from "./SteelFrameModel";
 export { default as StrongFloorModel } from "./StrongFloorModel";
+export { default as BasementModel } from "./BasementModel";
