55645a5386989d2bee6087cdae66e0d413b907f5
Lawrence Berry <lawrenceberry@outlook.com>
2020-04-29 18:45:36 +0100

    Views within a Text must have a width and height on Android

--- frontend/app/src/components/utils.js
+++ frontend/app/src/components/utils.js
@@ -1,11 +1,13 @@
 import React from "react";
+import { View, Platform } from "react-native";
 import { TSpan } from "react-native-svg";
 
 const formatTimestampLabel = (value) => {
   const datetime = new Date(value).toUTCString();
   const date = datetime.slice(5, 16);
   const time = datetime.slice(17, -4);
-  return (
+
+  const renderLabel = () => (
     <>
       <TSpan>{time}</TSpan>
       <TSpan dx="-4.3em" dy="1.2em">
@@ -13,6 +15,11 @@ const formatTimestampLabel = (value) => {
       </TSpan>
     </>
   );
+
+  return Platform.select({
+    default: <View style={{ width: 10, height: 10 }}>{renderLabel()}</View>,
+    ios: renderLabel(),
+  });
 };
 
 export default formatTimestampLabel;
