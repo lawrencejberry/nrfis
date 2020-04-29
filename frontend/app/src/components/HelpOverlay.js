77eaa16a74bcabe4aa6be94581c2bbeaa7f0a21c
Lawrence Berry <lawrenceberry@outlook.com>
2020-04-29 16:47:26 +0100

    Make help overlay layout responsive

--- frontend/app/src/components/HelpOverlay.js
+++ frontend/app/src/components/HelpOverlay.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import {
   View,
+  ScrollView,
   Modal,
   Animated,
   TouchableWithoutFeedback,
@@ -80,7 +81,7 @@ export default function HelpOverlay({ isActive, setIsActive }) {
                 flexDirection: "column",
               }}
             >
-              <HelpText style={{ alignSelf: "flex-end", marginRight: 260 }}>
+              <HelpText style={{ alignSelf: "flex-end", marginRight: 280 }}>
                 Shows whether the system is currently recording
               </HelpText>
             </View>
@@ -92,90 +93,68 @@ export default function HelpOverlay({ isActive, setIsActive }) {
                   alignItems: "flex-end",
                 }}
               >
-                <HelpText
-                  style={{
-                    position: "absolute",
-                    top: 20,
-                    right: 20,
-                    marginLeft: 40,
-                  }}
-                >
+                <HelpText style={{ flex: 1, marginHorizontal: 10 }}>
                   Model mode renders a 3D model of the building with coloured
                   sensors, "Chart" mode renders a scatter plot of the data
                 </HelpText>
-                <HelpText
-                  style={{
-                    position: "absolute",
-                    top: 100,
-                    right: 20,
-                  }}
-                >
+                <HelpText style={{ flex: 1, marginHorizontal: 10 }}>
                   Select the data you wish to visualise
                 </HelpText>
-                <View
-                  style={{
-                    height: "auto",
-                    padding: 20,
-                    marginHorizontal: 20,
-                    marginBottom: 20,
-                    backgroundColor: "white",
-                    borderRadius: 4,
-                  }}
-                >
-                  <Text>
-                    The National Research Facility for Infrastructure Sensing
-                    (NRFIS) is a new state of the art research facility hosted
-                    by the University of Cambridge. Housed in the new Civil
-                    Engineering Building, NRFIS brings together specialist
-                    engineering facilities and sensor development capabilities
-                    under one roof. It offers an interdisciplinary centre for
-                    cutting edge research to explore the development and
-                    application of novel sensor systems at a range of scales.
-                  </Text>
-                  <Text>
-                    The building is instrumented with six sensor packages, from
-                    the roof to the foundations. The sensors are an integral
-                    part of research being undertaken in Civil Engineering at
-                    Cambridge and link closely to the Centre for Smart
-                    Infrastructure and Construction (CSIC), also at the
-                    University of Cambridge. We are developing the technologies
-                    to display, store, interpret, and visualise these data
-                    streams. This information will be used to understand the
-                    performance of the new research facility and assess this
-                    performance against the predictions made during design. By
-                    examining any differences, we aim to understand performance,
-                    and help improve future design.
-                  </Text>
-                  <Text>
-                    This app serves as an interface into the building's fibre
-                    optic FBG sensor monitoring system, which continuously
-                    records strain and temperature change in the steel frame,
-                    basement and strong floor. It offers a platform for
-                    visualising the historical archive of data on interactive 3D
-                    models and scatter plots, as well as real-time data when the
-                    system is currently recording.
-                  </Text>
-                </View>
-                <View
-                  style={{
-                    height: "auto",
-                    padding: 20,
-                    marginHorizontal: 20,
-                    marginBottom: 90,
-                    backgroundColor: "white",
-                    borderRadius: 4,
-                  }}
-                >
-                  <Text>Sensor naming conventions:</Text>
-                  <Text>
-                    Steel Frame: [BM/CL = Beam/Column] - [DX = Grid No.] - [X =
-                    Floor] [a/b = Top/Bottom Flange]
-                  </Text>
-                  <Text>
-                    Basement + Strong Floor: [EW/NS = East-West/North-South] -
-                    [Str/Tmp = Strain/Temperature] - [bot/top = Bottom/Top] [X =
-                    Index on Cable]
-                  </Text>
+                <View style={{ flex: 6, margin: 20 }}>
+                  <View
+                    style={{
+                      height: "auto",
+                      padding: 20,
+                      marginBottom: 20,
+                      backgroundColor: "white",
+                      borderRadius: 4,
+                    }}
+                  >
+                    <Text>
+                      The National Research Facility for Infrastructure Sensing
+                      (NRFIS) is a new state of the art research facility hosted
+                      by the University of Cambridge. Housed in the new Civil
+                      Engineering Building, NRFIS brings together specialist
+                      engineering facilities and sensor development capabilities
+                      under one roof. The building is instrumented with six
+                      sensor packages, from the roof to the foundations. We are
+                      developing the technologies to display, store, interpret,
+                      and visualise these data streams. This information will be
+                      used to understand the performance of the new research
+                      facility and assess this performance against the
+                      predictions made during design. By examining any
+                      differences, we aim to understand performance, and help
+                      improve future design.
+                    </Text>
+                    <Text>
+                      This app serves as an interface into the building's fibre
+                      optic FBG sensor monitoring system, which continuously
+                      records strain and temperature change in the steel frame,
+                      basement and strong floor. It offers a platform for
+                      visualising the historical archive of data on interactive
+                      3D models and scatter plots, as well as real-time data
+                      when the system is currently recording.
+                    </Text>
+                  </View>
+                  <View
+                    style={{
+                      height: "auto",
+                      padding: 20,
+                      backgroundColor: "white",
+                      borderRadius: 4,
+                    }}
+                  >
+                    <Text>Sensor naming conventions:</Text>
+                    <Text>
+                      Steel Frame: [BM/CL = Beam/Column] - [DX = Grid No.] - [X
+                      = Floor] [a/b = Top/Bottom Flange]
+                    </Text>
+                    <Text>
+                      Basement + Strong Floor: [EW/NS = East-West/North-South] -
+                      [Str/Tmp = Strain/Temperature] - [bot/top = Bottom/Top] [X
+                      = Index on Cable]
+                    </Text>
+                  </View>
                 </View>
               </View>
               <View
--- frontend/app/src/utils/theme.js
+++ frontend/app/src/utils/theme.js
@@ -1,6 +1,7 @@
 import { Platform } from "react-native";
 
 const font = Platform.select({ default: "Roboto", ios: "Helvetica" });
+const fontSize = Platform.select({ default: 10, ios: 16 });
 
 const colors = {
   primary: "grey", // "#737f8a" - Light grey
@@ -14,7 +15,7 @@ const theme = {
   colors: colors,
   Text: {
     style: {
-      fontSize: 16,
+      fontSize: fontSize,
       fontFamily: font,
       fontWeight: "200",
       marginHorizontal: 16,
@@ -33,14 +34,14 @@ const theme = {
     },
     buttonStyle: { borderWidth: 1 },
     titleStyle: {
-      fontSize: 16,
+      fontSize: fontSize,
       fontFamily: font,
       color: colors.secondary,
     },
   },
   ButtonGroup: {
     textStyle: {
-      fontSize: 16,
+      fontSize: fontSize,
       fontFamily: font,
       fontWeight: "normal",
       color: colors.secondary,
